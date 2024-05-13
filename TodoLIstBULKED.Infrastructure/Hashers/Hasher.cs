c926520c844b54f0f82dcc121998e309e8257b93
OSUKAL <floky798@gmail.com>
2024-05-13 20:40:34 +1000

    Добавил хешер строк

---
+++ TodoLIstBULKED.Infrastructure/Hashers/Hasher.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+
+namespace TodoLIstBULKED.Infrastructure.Hashers;
+
+/// <inheritdoc/>
+public class Hasher : IHasher           
+{
+    private const int SaltLength = 16;
+    private const int HashLength = 16;
+    private const int Iterations = 1337;
+    private const char Separator = '-';
+    
+    private readonly HashAlgorithmName AlgorithmName = HashAlgorithmName.SHA512;
+    
+    /// <inheritdoc/>
+    public string Hash(string input)
+    {
+        var salt = RandomNumberGenerator.GetBytes(SaltLength);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(input, salt, Iterations, AlgorithmName, HashLength);
+
+        return string.Join(Separator, Convert.ToHexString(hash), Convert.ToHexString(salt), Iterations, AlgorithmName);
+    }
+
+    /// <inheritdoc/>
+    public bool HashCompare(string input, string hashedString)
+    {
+        var segments = hashedString.Split(Separator);
+        var hash = Convert.FromHexString(segments[0]);
+        var salt = Convert.FromHexString(segments[1]);
+        var iterations = int.Parse(segments[2]);
+        var algorithm = new HashAlgorithmName(segments[3]);
+        var hashedInput = Rfc2898DeriveBytes.Pbkdf2(input, salt, iterations, algorithm, hash.Length);
+        
+        return CryptographicOperations.FixedTimeEquals(hashedInput, hash);
+    }
+}
---
+++ TodoLIstBULKED.Infrastructure/Hashers/IHasher.cs
@@ -0,0 +1,22 @@
+﻿namespace TodoLIstBULKED.Infrastructure.Hashers;
+
+/// <summary>
+/// Хешер строк
+/// </summary>
+public interface IHasher
+{
+    /// <summary>
+    /// Хеширование строки
+    /// </summary>
+    /// <param name="input">Хешируемая строка</param>
+    /// <returns></returns>
+    string Hash(string input);
+
+    /// <summary>
+    /// Сравнение хеша строки со строкой
+    /// </summary>
+    /// <param name="input">Строка для сравнения</param>
+    /// <param name="hash">Хеш строки</param>
+    /// <returns></returns>
+    bool HashCompare(string input, string hash);
+}
